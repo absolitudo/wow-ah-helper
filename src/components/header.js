140530f7a0186712a6dc17b56f931863bf9ab8ea
absolitudo <absolitudo@gmail.com>
2017-10-25 14:45:34 +0200

    new style rules + info icon added

--- src/components/header.js
+++ src/components/header.js
@@ -2,12 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 
 /* Components */
+import InfoCircle from './infoCircle'
 import GetAuctionData from './getAuctionData'
 
 const Header = (props) => (
     <header>
         <GetAuctionData />
+        <InfoCircle/>
     </header>
+    
 )
 
 const mapStateToProps = (state) => state
