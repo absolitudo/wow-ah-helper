d773031fb4bfc420e1820fc4ade9b69f7e59b5fa
absolitudo <absolitudo@gmail.com>
2017-11-02 17:02:13 +0100

    introduction of the webapp in showmodal

--- src/components/infoModal.js
+++ src/components/infoModal.js
@@ -10,7 +10,20 @@ const InfoModal = (props) => (
     <div className='info-modal-container' onClick={props.toggleShowInfoModal}>
         {console.log('infoModal.js rendering')}
         <section className='info-modal' onClick={handleModalClick}>
-            info modal
+            <button onClick={props.toggleShowInfoModal}>X</button>
+            <h4>
+                This web app calculates which recipes in World of Warcraft makes profit by buying the ingredients from the auction house.
+            </h4>
+            <p>
+                It uses the data of <a href="https://wow.curseforge.com/projects/auctioneer/files" target='_blank' rel="noopener noreferrer">Auctioneer</a> addon.
+                You have to scan the auction house with the addon to extract data with it. You can use this data to calculate which recipes are worth crafting.
+                The auction data is called "Auc-ScanData.lua" and it can be found at 
+                <code>
+                    "<span className='hightlight-span'>{'{YOURWOWCLIENT}'}</span>/WTF/Account/<span className='hightlight-span'>{'{YOURACCOUNTNAME}'}</span>/SavedVariables/Auc-ScanData.lua".
+                </code>
+            </p>
+
+
 
         </section>
     </div>
