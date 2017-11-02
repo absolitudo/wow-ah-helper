a40f7e7abc1496a846f5c62e512f3a513aff73cf
absolitudo <absolitudo@gmail.com>
2017-11-02 16:35:46 +0100

    basic styles and onclicks on modal

--- src/components/infoModal.js
+++ src/components/infoModal.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+/* Action creators */
+import { toggleShowInfoModal } from '../redux/actions'
+
 
 const InfoModal = (props) => (
-    <div className='info-modal'>
+    <div className='info-modal-container' onClick={props.toggleShowInfoModal}>
         {console.log('infoModal.js rendering')}
-        info modal
+        <section className='info-modal' onClick={handleModalClick}>
+            info modal
+
+        </section>
     </div>
 )
 
-export default InfoModal
+const handleModalClick = (event) => {
+    event.stopPropagation()
+    return false
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators({ toggleShowInfoModal }, dispatch)
+
+export default connect(null, mapDispatchToProps)(InfoModal)
