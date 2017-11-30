7039d7de3b16bc447bdfdc556553e95b1c3e1c42
absolitudo <absolitudo@gmail.com>
2017-11-30 22:47:34 +0100

    item contents, sell price is not yet complete

--- src/components/item.js
+++ src/components/item.js
@@ -4,8 +4,30 @@ const Item = (props) => (
     <div className='item'>
         <div className='item-left'>
             <div className="item-left-upper">
-                item name
-                item description
+                <div className="item-header">
+                    <img src={'https://my-wow-api.herokuapp.com/icon?name=' + props.item.iconName + '.png'}
+                        alt='Icon of the item'
+                        />
+                    <h2>
+                        <a href={'http://mop-shoot.tauri.hu/?item=' + props.item.id}
+                            rel='noreferrer noopener'
+                            target='_blank'
+                            className={'item-quality-' + props.item.quality}
+                            >
+                            {props.item.itemName}
+                        </a>
+                    </h2>
+
+                </div>
+                <div className="item-content">
+                    {props.item.tooltip.castTime && <p>Cast Time: {props.item.tooltip.castTime}</p>}
+                    {props.item.tooltip.itemLevel && <p className='item-level'>Item Level {props.item.tooltip.itemLevel}</p>}
+                    {props.item.tooltip.description.map((description, index) => (
+                        <p key={index}>{description}</p>
+                    ))}
+                    {props.item.tooltip.requiredLevel && <p>Requires Level {props.item.tooltip.requiredLevel}</p>}
+                    {props.item.tooltip.vendorSellPrice && <p>Sell Price: {props.item.tooltip.vendorSellPrice}</p>}
+                </div>
             </div>
             <div className="item-left-lower">
                 item price shits
