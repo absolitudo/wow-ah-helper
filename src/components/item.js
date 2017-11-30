3ce3963e831fbeda15f67b24cda0b6a72dc34134
absolitudo <absolitudo@gmail.com>
2017-11-30 21:12:14 +0100

    item layout css

--- src/components/item.js
+++ src/components/item.js
@@ -4,15 +4,16 @@ const Item = (props) => (
     <div className='item'>
         <div className='item-left'>
             <div className="item-left-upper">
-
+                item name
+                item description
             </div>
             <div className="item-left-lower">
-                
+                item price shits
             </div>
         </div>
 
         <div className='item-right'>
-
+            item reagents
         </div>
     </div>
 )
