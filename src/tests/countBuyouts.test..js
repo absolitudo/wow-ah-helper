9147d6b765a194c37deb7762eb87c6a2779323f6
absolitudo <absolitudo@gmail.com>
2017-12-04 17:13:36 +0100

    test for one price multiple times and one price once then an other once added

--- src/tests/countBuyouts.test..js
+++ src/tests/countBuyouts.test..js
@@ -1,12 +1,32 @@
 import { countBuyouts } from '../components/priceChart'
 
-let oneBuyoutData = [4]
+let onePriceOnce = [4]
+let onePriceMultileTimes = [4, 4, 4, 4]
+let onePriceOnePrice = [2, 4]
 
 describe('Count buyouts:', () => {
-    it('one number', () => {
-        expect(countBuyouts(oneBuyoutData)).toBe({
+    it('one price once', () => {
+        expect(countBuyouts(onePriceOnce)).toBe([{
             price: 4,
             amount: 1
-        })
+        }])
     })
+
+    it('one price multiple times', () => {
+        expect(countBuyouts(onePriceMultileTimes)).toBe([{
+            price: 4,
+            amount: 4
+        }])
+    })
+
+    it('one price once, an other price once', () => {
+        expect(countBuyouts(onePriceOnePrice)).toBe([{
+            price: 2,
+            amount: 1
+        },{
+            price: 4,
+            amount: 1
+        }])
+    })
+
 })
