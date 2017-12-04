83e1659bca1b9d646209edf3eafae128be59c486
absolitudo <absolitudo@gmail.com>
2017-12-04 17:05:02 +0100

    trying test first approach

--- src/components/priceChart.js
+++ src/components/priceChart.js
@@ -4,69 +4,40 @@ import * as d3 from 'd3'
 
 class PriceChart extends React.Component {
     componentDidMount() {
-        const createData = (data) => {
+        const width = 290
+        const height = 50
+        const margin = 10
+        const data = countBuyouts(this.props.data)
 
-            let newData = []
-
-            let num = data[0]
-            let counter = 1
-            newData.push({
-                price: num,
-                amount: counter
-            })
-            for (let i = 0; i < data.length; i += 1) {
-
-                if(num !== data[i]) {
-                    newData.push({
-                        price: num,
-                        amount: counter
-                    })
-
-                    num = data[i]
-                    counter = 1                    
-                } else {
-                    counter += 1
-                }
-            }
-
-            return newData
-        }
-
-        var width = 290
-        var height = 50
-        let svg = d3.select(this.refs.mysvg)
+        let svg = d3.select(this.refs.svg)
             .attr('width', width)
             .attr('height', height)
-
-        let data = createData(this.props.data)
-
-        console.log(data)
 
         /* Domains */
         let xDomain = d3.extent(data, (d) => d.price)
         let yDomain = d3.extent(data, (d) => d.amount)
 
+
         /* X scale */
         let xScale = d3.scaleLinear()
             .domain(xDomain[0], xDomain[1])
-            .range([0, width])
+            .range([margin, width - margin])
 
             /* Y scale */
         let yScale = d3.scaleLinear()
-            .domain([0, yDomain[1]]).nice()
-            .range([height, 0])
+            .domain([0, yDomain[1]])
+            .range([height - margin, margin])
 
 
         /* X axis */
         let xAxis = d3.axisBottom()
             .scale(xScale)
-            .ticks(8)
 
         /* Y axis */
-        let yAxis = d3.axisRight()
+        let yAxis = d3.axisLeft()
             .scale(yScale)
-            .ticks(8)
-            .tickSize(width)
+            .tickSize(width - margin * 2)
+            .ticks(4)
 
         /* Color scale */
         let colorScale = d3.scaleLinear()
@@ -76,29 +47,35 @@ class PriceChart extends React.Component {
 
         /* Groups */
         svg.append("g")
-            .attr("transform", "translate(0," + height +")")
+            .attr("transform", "translate(0," + (height - margin) +")")
             .call(xAxis)
 
         svg.append("g")
+            .attr("transform", "translate(" + width - margin  +", 0)")
             .call(yAxis)
+
         svg.append("g")
             .selectAll("rect")
             .data(data)
             .enter()
                 .append("rect")
                     .attr("fill", (d) => colorScale(d.price))
-                    .attr("width", width / data.length)
-                    .attr("height", (d) => height - (yScale(d.amount)))
-                    .attr("x", (d, i ) => i * (width / data.length))
-                    .attr("y", (d) => yScale(d.amount))
+                    .attr("width", (width - margin * 2) / data.length)
+                    .attr("height", (d) => (height - margin * 2)  - (yScale(d.amount)))
+                    .attr("x", (d, i) => margin + i * (width / data.length))
+                    .attr("y", (d) => margin + yScale(d.amount))
     }
 
     render() {
         return (
-            <svg ref='mysvg'>
+            <svg ref='svg'>
             </svg>
         )
     }
+}
+
+export const countBuyouts = (data) => {
+    return data
 }
 
 export default PriceChart
