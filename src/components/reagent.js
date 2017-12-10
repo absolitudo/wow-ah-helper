22bb1f579837bd9628d0c995e3bea3a20c8b8750
absolitudo <absolitudo@gmail.com>
2017-12-10 18:28:32 +0100

    reagent price display, not entirely done yet

--- src/components/reagent.js
+++ src/components/reagent.js
@@ -8,9 +8,13 @@ const Reagent = (props) => (
     <div>
         <ItemInfo item={props.reagent} tooltip={true} />
         {(props.reagent.chartData !== false && props.reagent.prices) && (
-            <div>
-                <PriceChart data={props.reagent.chartData} amount={props.reagent.prices.amount} />
-            </div>
+            <PriceChart data={props.reagent.chartData} amount={props.reagent.prices.amount} />
+        )}
+        {props.reagent.prices && (
+            <p>
+                Reagent price: 
+                <input type="number" value={props.reagent.prices.calculationPrice} />
+            </p>
         )}
     </div>
 )
